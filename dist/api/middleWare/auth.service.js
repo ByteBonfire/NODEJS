3a9ae08a3d19588f5ff5bb3c36cdc6708b6a2d96
Saroj Chettri <sarojbc040@gmail.com>
2023-04-25 12:57:00 +0545

    modified the gitignore file

--- dist/api/middleWare/auth.service.js
+++ dist/api/middleWare/auth.service.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const crypto = require("crypto");
 const config_1 = __importDefault(require("../../config/config"));
 const jwt = require("jsonwebtoken");
 class authservice {
